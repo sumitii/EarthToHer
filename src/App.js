d2b654866943d28289c8b4ed068a420ef8b33f40
Sumita Mukund <sumitamukund@sumitamukund.local>
2018-02-18 13:03:41 -0800

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'flexboxgrid/css/flexboxgrid.min.css'; 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='main row'>
+        <div className='main__header col-xs-12'>
+          <h2>Earth to Her</h2>
+        </div>
+        
       </div>
     );
   }
