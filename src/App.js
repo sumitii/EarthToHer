61c1a16f812277057a8f7765d05c617ad191434a
Sumita Mukund <sumitamukund@sumitamukund.local>
2018-02-18 13:41:01 -0800

    homepage site styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import 'flexboxgrid/css/flexboxgrid.min.css'; 
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className='main row'>
-        <div className='main__header col-xs-12'>
+        <div className='main__leftSidebar col-xs-1' />
+        <div className='main__header col-xs-11'>
           <h2>Earth to Her</h2>
         </div>
         
