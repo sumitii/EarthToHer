aa531c8afd016e7882819ccf87e8cb84dd984a4d
Sumita Mukund <sumitamukund@sumitamukund.local>
2018-02-26 13:01:56 -0800

    homepage animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import 'flexboxgrid/css/flexboxgrid.min.css'; 
+import 'flexboxgrid/css/flexboxgrid.min.css';
+import faker from 'faker';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className='main row'>
-        <div className='main__leftSidebar col-xs-1' />
-        <div className='main__header col-xs-11'>
-          <h2>Earth to Her</h2>
+        <div className='main__slide-in'>
+          <div className='main__video'>
+            <img src={faker.image.image()} alt='earth-to-her-video' />
+          </div>
         </div>
-        
+        <div className='main__wrapper'>
+          <div className='main__leftSidebar col-xs-2 col-sm-1' />
+          <div className='main__header col-xs-10 col-sm-11'>
+            <h2>Earth to Her</h2>
+          </div>
+        </div>
+
       </div>
     );
   }
